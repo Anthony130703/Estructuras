52e3d03eeb7367e8a9c10d325248f93014dca1f4
Anthony <tonynarciso2003@gmail.com>
2024-10-21 14:03:03 -0500

    probando arreglo dinamico

--- struct-anidadas.cpp
+++ struct-anidadas.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <limits>  // Biblioteca necesaria para numeric_limits
+#include <stdlib.h>  //BIBLIOTECA PARA CREAR ARREGLO DINAMICO
+
 using namespace std;
 
 
@@ -15,40 +17,57 @@ struct empleado
     char name[40];
     struct inf_address dir_empleado;
     double salario;
-}empleado[2];
+};
 
 int main()
 {
-    for(int i=0;i<2;i++)
+    int numEmpleados=0;
+    cout << "Ingrese el numero de emppleados a registrar: ";
+    cin >> numEmpleados;
+
+    //LIMPIAR EL BUFFER DE ENTRADA DESPUES DE RESIVIR EL NUMERO
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    //CREANDO EL ARREGLO DINAMICO
+    empleado* empleados = new empleado[numEmpleados];
+
+    cout << endl;
+
+    for(int i=0;i<numEmpleados;i++)
     {
         cout << "Empleado " << i+1 << endl;
         cout << "Ingrese el nombre: ";
-        cin.getline(empleado[i].name, 40, '\n');
+        cin.getline(empleados[i].name, 40, '\n');
         cout << "Ingrese la direccion: ";
-        cin.getline(empleado[i].dir_empleado.address, 50, '\n');
+        cin.getline(empleados[i].dir_empleado.address, 50, '\n');
         cout << "Ingrese la ciudad: ";
-        cin.getline(empleado[i].dir_empleado.city, 50, '\n');
+        cin.getline(empleados[i].dir_empleado.city, 50, '\n');
         cout << "Ingrese la provincia: ";
-        cin.getline(empleado[i].dir_empleado.province, 40, '\n');
+        cin.getline(empleados[i].dir_empleado.province, 40, '\n');
         cout << "Ingrese el salario: ";
-        cin >> empleado[i].salario;
+        cin >> empleados[i].salario;
+
         // Limpiar el buffer de entrada para evitar problemas con getline
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Esta línea es la clave
+
         cout << endl;
 
     }
 
     //IMPRIMIENDO LOS DATOS INGRESADOS
-    for(int i=0;i<2;i++)
+    for(int i=0;i<numEmpleados;i++)
     {
         cout << "Empleado " << i+1 << endl;
-        cout << "Nombre: " << empleado[i].name << endl;
-        cout << "Direccion: " << empleado[i].dir_empleado.address << endl;
-        cout << "Ciudad: " << empleado[i].dir_empleado.city << endl;
-        cout << "Provincia: " << empleado[i].dir_empleado.province << endl;
-        cout << "Salario: " << empleado[i].salario << endl;
+        cout << "Nombre: " << empleados[i].name << endl;
+        cout << "Direccion: " << empleados[i].dir_empleado.address << endl;
+        cout << "Ciudad: " << empleados[i].dir_empleado.city << endl;
+        cout << "Provincia: " << empleados[i].dir_empleado.province << endl;
+        cout << "Salario: " << empleados[i].salario << endl;
         cout << endl;
     }
+
+    //LIMPIANDO LA MEMORIA ASIGANAD DINAMICAMENTE
+    delete[] empleados;
     
     return 0;
 }
